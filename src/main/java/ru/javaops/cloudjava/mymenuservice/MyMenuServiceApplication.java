1be5641acbe11a4df859ba9b94d12a4c30d311c1
romalxr <romalxr@gmail.com>
2024-10-09 16:46:36 +0300

    Initial commit

---
+++ src/main/java/ru/javaops/cloudjava/mymenuservice/MyMenuServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.javaops.cloudjava.mymenuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMenuServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/javaops/cloudjava/mymenuservice/MyMenuserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaops.cloudjava.mymenuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMenuserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
